d7271a3ded74f306b2ebec30b5280328aa5929a6
rakipovich <damir.khairulin@delvedeeper.com>
2021-07-25 12:33:46 +0300

    Testing frameworks

--- meadowlark.js
+++ meadowlark.js
@@ -10,6 +10,11 @@ app.set('view engine', "handlebars");
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+})
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res) {
@@ -17,7 +22,10 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-    res.render('about', { fortune: fortune.getFortune() })
+    res.render('about', { 
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/test-about.js'
+    });
 });
 
 // custom 404 page
